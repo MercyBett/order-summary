0293c856ee4b4b50edb4b7175d10643b0ab28c7b
Mercybett <merjep551@gmail.com>
2021-12-01 22:18:14 +0300

    add custom font and color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import music from "./img/music.svg";
+import hero from "./img/hero.svg";
+import mobile from "./img/mobile.svg";
+import desktop from "./img/desktop.svg";
 
 function App() {
   return (
-    <div className="flex">
-      <img src={music} alt="" />
+    <div className="flex justify-center items-center">
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <img src={music} alt="" />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      pale: "hsl(225, 100%, 94%)",
+      bright: "hsl(245, 75%, 52%)",
+      veryPale: "hsl(225, 100%, 98%)",
+      desaturated: "hsl(224, 23%, 55%)",
+      dark: "hsl(223, 47%, 23%)",
+    },
+    extend: {
+      fontFamily: {
+        name: ["Red Hat Display", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
